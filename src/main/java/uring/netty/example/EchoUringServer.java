bd7f1cd47ca10555168fd795f8b5420cc2b2bfc3
Josef Grieb <josef.grieb@gmail.com>
2020-12-19 02:54:43 +0100

    First Commit

---
+++ src/main/java/uring/netty/example/EchoUringServer.java
@@ -0,0 +1,79 @@
+package uring.netty.example;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.incubator.channel.uring.IOUringEventLoopGroup;
+import io.netty.incubator.channel.uring.IOUringServerSocketChannel;
+
+public class EchoUringServer {
+
+    private static int PORT = 2022;
+
+    @ChannelHandler.Sharable
+    public static class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+            ctx.write(msg);
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) {
+            ctx.flush();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            // Close the connection when an exception is raised.
+            ctx.close();
+        }
+
+        @Override
+        public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+            // Ensure we are not writing to fast by stop reading if we can not flush out data fast enough.
+            if (ctx.channel().isWritable()) {
+                ctx.channel().config().setAutoRead(true);
+            } else {
+                ctx.flush();
+                if (!ctx.channel().isWritable()) {
+                    ctx.channel().config().setAutoRead(false);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String [] args) {
+        EventLoopGroup workerGroup = new IOUringEventLoopGroup(1);
+        EventLoopGroup bossGroup = new IOUringEventLoopGroup(1);
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .option(ChannelOption.SO_REUSEADDR, true)
+                    .channel(IOUringServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            //p.addLast(new LoggingHandler(LogLevel.INFO));
+                            p.addLast(serverHandler);
+                        }
+                    });
+
+            // Start the server.
+            ChannelFuture f = b.bind(PORT).sync();
+
+            // Wait until the server socket is closed.
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // Shut down all event loops to terminate all threads.
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    }
